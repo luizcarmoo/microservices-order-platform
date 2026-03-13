754c46bc1181c39f36dab38be8b9e0460e0adfcc
luizcarmoo <luiz.pcarmo@gmail.com>
2026-03-13 15:25:04 -0300

    feat(resilience): implement circuit breaker and fallback for auth-service in API Gateway

--- apigateway/src/main/java/com/luizcarmo/apigateway/controller/FallbackController.java
+++ apigateway/src/main/java/com/luizcarmo/apigateway/controller/FallbackController.java
@@ -3,16 +3,28 @@ package com.luizcarmo.apigateway.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
 
-    @GetMapping("/auth")
-    public ResponseEntity<String> authFallback() {
+    @RequestMapping("/auth")
+    public ResponseEntity<Map<String, Object>> authFallback(ServerHttpRequest request) {
+
+        Map<String, Object> response = Map.of(
+                "service", "AUTH-SERVICE",
+                "status", "DOWN",
+                "path", request.getURI().getPath(),
+                "message", "Authentication service temporarily unavailable",
+                "timestamp", LocalDateTime.now()
+        );
 
         return ResponseEntity
                 .status(HttpStatus.SERVICE_UNAVAILABLE)
-                .body("Auth Service is temporarily unavailable. Please try again later.");
+                .body(response);
     }
 }
